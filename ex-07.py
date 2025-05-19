6c06e76ee8c708c969a18f42ba6ab3b7870703ea
sm0307-2008 <samgabraf@gmail.com>
2025-05-19 19:00:19 -0300

    Update ex-07.py

--- ex-07.py
+++ ex-07.py
@@ -1,3 +1,4 @@
+#7) Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores
 total_eleitores = int(input('Numero total de eleitores: '))
 voto_branco = int(input('Numero total de votos brancos: '))
 voto_nulo = int(input('Numero total de votos nulos: '))
